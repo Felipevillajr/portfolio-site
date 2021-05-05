546bbc10151464ca8b4423325737123de717710e
felipevillajr <felipevilla2010@live.com>
2021-05-05 15:41:42 -0400

    removed unused imports in header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Header.scss";
-import logo from "../../Assets/IMGS/web_design_logo.png";
-import { Link } from "react-router-dom";
 
 export default function Header() {
   return <div></div>;
