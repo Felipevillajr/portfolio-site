59718347237e0df65c0c3bbbd42922378bd44447
felipevillajr <felipevilla2010@live.com>
2021-05-07 20:49:02 -0400

    added services page

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Services.scss";
+import underLogo from "../../Assets/LOGO/underconstruction_logo.png";
 
 export default function Services() {
-  return <div></div>;
+  return (
+    <div className="services">
+      <p className="services__text">
+        This Page is still under contruction. Thank you for you consideration.
+      </p>
+    </div>
+  );
 }
